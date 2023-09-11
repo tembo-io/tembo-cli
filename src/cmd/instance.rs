131343823cf8f9d6062549647055dd9119491109
Mike Krisher <mike@mikekrisher.com>
2023-09-11 10:42:53 -0400

    Cleanup

--- src/cmd/instance.rs
+++ src/cmd/instance.rs
@@ -7,9 +7,7 @@ pub mod create {
     use crate::cli::stacks::Stacks;
     use chrono::prelude::*;
     use clap::{Arg, ArgAction, ArgMatches, Command};
-    //use spinners::{Spinner, Spinners};
     use std::error::Error;
-    //use std::process::Command as ShellCommand;
 
     // example usage: tembo instance create -t oltp -n my_app_db -p 5432
     pub fn make_subcommand() -> Command {
@@ -63,7 +61,7 @@ pub mod create {
                 }
             }
 
-            match persist_instance_config(&matches) {
+            match persist_instance_config(matches) {
                 Ok(_) => println!("- Instance config persisted in config file"),
                 Err(e) => {
                     eprintln!("{}", e);
